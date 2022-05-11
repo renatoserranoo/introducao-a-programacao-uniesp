4dddc44183763aea3c423269f3248770b1f59a82
renatoserranoo <renatoserranoribeiro@gmail.com>
2022-05-11 20:31:54 -0300

    dicionarios

---
+++ dicionarios2.py
@@ -0,0 +1,11 @@
+glossario = {
+    'strings': 'Strings são cadeias de caracteres que armazenam dados textuais.',
+    'algoritmo': 'Algoritmo é uma sequência finita de ações executáveis que visam obter uma solução para um determinado tipo de problema.',
+    'webscraping': 'Web Scraping é uma técnica utilizada para extrair rapidamente informações de sites e exportá-las para planilhas com o intuito de fazer análises e gerar insights para tomadas de decisão.',
+    'boolean': 'Boolean é um tipo de dado lógico que pode ter apenas um de dois valores possíveis: verdadeiro ou falso.',
+    'python': 'Python é uma linguagem Open-Source de propósito geral usado bastante em data science, machine learning, desenvolvimento de web, desenvolvimento de aplicativos, automação de scripts.'
+}
+print('Palavras na programação')
+print('------------------------------------')
+for i, e in glossario.items():
+    print(i + ':', e, '\n')
